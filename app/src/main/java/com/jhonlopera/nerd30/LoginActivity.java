3a362efca83ccb7d9eae6827ba9d8c7236aea0e5
jhonlopera05 <jhon.lopera05@gmail.com>
2017-11-28 12:51:26 -0500

    Proyecto Final

--- app/src/main/java/com/jhonlopera/nerd30/LoginActivity.java
+++ app/src/main/java/com/jhonlopera/nerd30/LoginActivity.java
@@ -3,15 +3,10 @@ package com.jhonlopera.nerd30;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -31,9 +26,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -81,85 +73,8 @@ public class LoginActivity extends AppCompatActivity {
         nivelcon=preferencias.getInt("nivelcon",1);
         niveltopo=preferencias.getInt("niveltopo",1);
 
-        //-------------------------------------------------------
-        //Si el logggin es con el registro de usuario
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-
-            correoR = extras.getString("correo");
-            contraseñaR = extras.getString("contraseña");
-            nombreR = extras.getString("nombre");
-
-        }
-        //--------------------------------------------------------------------
-
-        //Login con facebook
-        /*
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("public_profile"); //Permisos del perfil publico
-        callbackManager = CallbackManager.Factory.create();
-
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                log = "facebook";
-                silog = 1;
-                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                        if (response.getError() != null) {
 
 
-                        } else {
-                            try {
-                                nombreR = object.getString("name");
-                                correoR = object.getString("email");
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-
-                            Profile profile = com.facebook.Profile.getCurrentProfile();
-                            urifoto = profile.getProfilePictureUri(400, 400); //foto de tamaño 400x400
-
-                            if ((urifoto == null))
-                                foto = null;
-                            else
-                                foto = urifoto.toString();
-
-                            guardarPreferencias(silog, correoR, nombreR, foto, log);
-                            //prueba = preferencias.getString("nombre", "noseobtuvo");
-                           // Toast.makeText(getApplicationContext(), prueba, Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(LoginActivity.this, PrincipalActivity.class);
-                            //intent.putExtra("correo", correoR);
-                            //intent.putExtra("nombre", nombreR);
-                            //intent.putExtra("foto", foto);
-                            //intent.putExtra("log", log);
-                            startActivity(intent);
-                            finish();
-                        }
-                    }
-                });
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,email,gender");
-                request.setParameters(parameters);
-                request.executeAsync();
-            }
-
-            @Override
-            public void onCancel() {
-                Toast.makeText(getApplicationContext(), "Login  cancelado", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                Toast.makeText(getApplicationContext(), "Error en el login", Toast.LENGTH_SHORT).show();
-            }
-        });
-*/
-        //Para loggin con google
-        //-------------------------------------------------------------------------------------------
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail()
                 .requestProfile().build();
 
@@ -173,54 +88,27 @@ public class LoginActivity extends AppCompatActivity {
         SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
 
+        signIn();
+
         signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
+           @Override
             public void onClick(View view) {
                 signIn();
             }
         });
-        //-------------------------------------------------------------------------------------------
     }
 
-    public void iniciar(View view) {
 
-        correo = ecorreo.getText().toString();
-        contraseña = econtraseña.getText().toString();
-        log = "registro";
-        silog = 1;
-        contraseñaR = preferencias.getString("contraseña", "/((&!=");
-        correoR = preferencias.getString("correo", "/(/%#%//(%#");
-
-        if (correo.equals(correoR) && contraseña.equals(contraseñaR)) {
-            editor_preferencias.putInt("silog", silog);
-            editor_preferencias.putString("log", log);
-            editor_preferencias.commit();
-            Intent intent = new Intent(LoginActivity.this, PrincipalActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-            Context context = getApplicationContext();
-            CharSequence text = "Datos incorrectos";
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
-
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1234 && resultCode == RESULT_OK) {
-            //registro
-            prueba = preferencias.getString("nombre", "no se obtuvo");
-            Toast.makeText(getApplicationContext(), prueba, Toast.LENGTH_SHORT).show();
-            correoR = preferencias.getString("correo", "no se obtivo");
-            contraseñaR = preferencias.getString("contraseña", "no se obtivo");
-            nombreR = preferencias.getString("nombre", "no se obtivo");
-        } else if (requestCode == RC_SIGN_IN) {//login con google
+        //if (requestCode == 1234 && resultCode == RESULT_OK)
+        if (requestCode == RC_SIGN_IN) {
+            //login con google
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
         } else {
-            //facebook
+            //login facebook
             callbackManager.onActivityResult(requestCode, resultCode, data);
         }
         super.onActivityResult(requestCode, resultCode, data);
@@ -349,14 +237,13 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             // Signed out, show unauthenticated UI.
             Toast.makeText(getApplicationContext(), "Verifique su conexión", Toast.LENGTH_SHORT).show();
+            //finish();
+           // System.exit(0);
         }
     }
 
 
-    public void Registrarse(View view) {
-        Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
-        startActivityForResult(intent, 1234);
-    }
+
 
     private void signIn() {
 
@@ -364,23 +251,7 @@ public class LoginActivity extends AppCompatActivity {
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
-    public void generarHash() {
 
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "com.jhonlopera.nerd30",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-
-        } catch (NoSuchAlgorithmException e) {
-        }
-
-    }
 
     void guardarPreferencias(int silog, String correo, String nombre, String foto, String log) {
         editor_preferencias.putInt("silog", silog);
@@ -390,6 +261,8 @@ public class LoginActivity extends AppCompatActivity {
         editor_preferencias.putString("log", log);
         editor_preferencias.commit();
     }
+
+
 
 
 }
